9778f21a319b9ee8ba734188a55ad55baabb68c1
Prem Kumar <118460551+premkr7370@users.noreply.github.com>
2024-08-18 00:45:55 +0530

    add script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+function getCheese(){
+      return new Promise((resolve,reject)=>{
+            setTimeout(()=>{ 
+                  const cheese="🧀";
+                  resolve(cheese);
+
+            },2000);
+      });
+}
+function makeDough(cheese){
+      return new Promise((resolve,reject)=>{
+            setTimeout(()=>{ 
+                  const dough=cheese+"🫓";
+                  resolve(dough);
+
+            },2000);
+      });
+}
+function bakePizza(dough){
+      return new Promise((resolve,reject)=>{
+            setTimeout(()=>{ 
+                  const pizza=dough+"🍕";
+                  resolve(pizza);
+
+            },2000);
+      });
+}
+async function orderPizza(){
+      try{
+            const cheese=await getCheese();
+            console.log("here is the cheese",cheese);
+            const dough=await makeDough(cheese);
+            console.log("here is the dough",dough);
+            const pizza=await bakePizza(dough);
+            console.log("here is the pizza",pizza);
+      }catch(err){
+            console.log("error occured",err);
+
+      }
+}
+orderPizza();
